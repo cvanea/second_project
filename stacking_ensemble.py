6d05e593ee7669cd90f0caddbcdca479b87694e5
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2020-07-20 16:56:15 +0100

    k-fold cross val working across time

--- stacking_ensemble.py
+++ stacking_ensemble.py
@@ -1,20 +1,19 @@
 import numpy as np
 import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from mlxtend.classifier import StackingClassifier
 
 from utils import get_y_train
 
 
 def main():
     base_model_type = "lda"
-    exp_name = "wavelet_class/lsqr/complex"
-    meta_model_type = 'log_reg'
+    base_model_dir = "wavelet_class/lsqr/complex"
+    save_dir = "Results/stacking_ensemble"
 
-    load_dir = "Results/{}/{}".format(base_model_type, exp_name)
-
-    freqs = np.logspace(*np.log10([2, 25]), num=15)
-    string_freqs = [str(round(x, 2)) for x in freqs]
+    load_dir = "Results/{}/{}".format(base_model_type, base_model_dir)
 
     all_sample_models = np.array(pickle.load(open(load_dir + "/all_models.pkl", "rb")))
 
@@ -24,6 +23,8 @@ def main():
         all_y_train.append(get_y_train(sample))
 
     all_x_train = pickle.load(open("DataTransformed/wavelet_complex/x_train_all_samples.pkl", "rb"))
+
+    results = np.zeros(50)
 
     for time in range(50):
         print("time {}".format(time))
@@ -39,8 +40,6 @@ def main():
 
             freq_proba = []
             for freq in range(15):
-                # all_x_train[sample][freq] = all_x_train[sample][freq][intervals, :]
-
                 base_x_train = all_x_train[sample][freq][intervals, :]
 
                 prediction_proba = sample_models[sample][freq].predict_proba(base_x_train)
@@ -58,24 +57,42 @@ def main():
             final.append([data for freq_data in sample_y_train[sample] for data in freq_data])
         sample_y_train = final
 
-        x_train = sample_predictions_proba[:-1]
-        x_train = [data for freq_data in x_train for data in freq_data]
-        print("length of x_train: {}".format(len(x_train)))
-        y_train = sample_y_train[:-1]
-        y_train = [data for freq_data in y_train for data in freq_data]
-        # print("length of y_train: {}".format(len(y_train)))
+        all_val_acc = []
 
-        x_val = sample_predictions_proba[-1:]
-        x_val = [data for freq_data in x_val for data in freq_data]
-        print("length of x_val: {}".format(len(x_val)))
-        y_val = sample_y_train[-1:]
-        y_val = [data for freq_data in y_val for data in freq_data]
-        # print("length of y_val: {}".format(len(y_val)))
+        for val_sample in range(21):
+            print("left out validation subject: {}".format(val_sample + 1))
 
-        meta_model = LogisticRegression()
-        meta_model.fit(x_train, y_train)
+            x_train = sample_predictions_proba[:val_sample] + sample_predictions_proba[val_sample + 1:]
+            x_train = [data for freq_data in x_train for data in freq_data]
+            y_train = sample_y_train[:val_sample] + sample_y_train[val_sample + 1:]
+            y_train = [data for freq_data in y_train for data in freq_data]
 
-        print(meta_model.score(x_val, y_val))
+            x_val = sample_predictions_proba[val_sample]
+            # x_val = [data for freq_data in x_val for data in freq_data]
+            y_val = sample_y_train[val_sample]
+            # y_val = [data for freq_data in y_val for data in freq_data]
+
+            meta_model = LogisticRegression()
+            meta_model.fit(x_train, y_train)
+
+            acc_score = meta_model.score(x_val, y_val)
+            all_val_acc.append(acc_score)
+
+        all_val_acc = np.array(all_val_acc)
+        avg_val_acc = np.mean(all_val_acc, axis=0)
+        print("average cross val score: {}".format(avg_val_acc))
+        results[time] = avg_val_acc
+
+    sns.set()
+    ax = sns.lineplot(data=results, dashes=False)
+    ax.set(ylim=(0, 1), xlabel='Time', ylabel='Accuracy',
+           title='Average Cross Val Accuracy Stacking Ensemble {} Base Models'.format(base_model_type))
+    plt.axvline(x=15, color='b', linestyle='--')
+    ax.figure.savefig("{}/LOOCV.png".format(save_dir), dpi=300)
+    plt.clf()
+
+    results_df = pd.DataFrame(results)
+    results_df.to_csv("{}/LOOCV.csv".format(save_dir))
 
 
 if __name__ == "__main__":

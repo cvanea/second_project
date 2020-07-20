872928937bfa32e597e01867413ca2c693cf4d4d
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2020-07-20 15:51:14 +0100

    working stacking ensemble for single val set

--- stacking_ensemble.py
+++ stacking_ensemble.py
@@ -30,40 +30,53 @@ def main():
 
         sample_models = all_sample_models[:, :, time]
 
-        sample_x_train = []
         sample_y_train = []
+        sample_predictions_proba = []
 
         for sample in range(21):
             intervals = np.arange(start=time, stop=all_y_train[sample].shape[0], step=50)
             sample_y_train.append(all_y_train[sample][intervals])
 
+            freq_proba = []
             for freq in range(15):
-                all_x_train[sample][freq] = all_x_train[sample][freq][intervals, :]
+                # all_x_train[sample][freq] = all_x_train[sample][freq][intervals, :]
 
-            sample_x_train.append(all_x_train[sample])
+                base_x_train = all_x_train[sample][freq][intervals, :]
 
-        sample_x_train = np.array(sample_x_train)
+                prediction_proba = sample_models[sample][freq].predict_proba(base_x_train)
+                freq_proba.append(prediction_proba)
+
+            sample_predictions_proba.append(freq_proba)
+
+        sample_predictions_proba = [np.vstack(sample).astype(np.float) for sample in sample_predictions_proba]
+
         sample_y_train = np.array(sample_y_train)
+        sample_y_train = np.repeat(sample_y_train[:, np.newaxis], 15, axis=1)
+        sample_y_train = [np.vstack(sample).astype(np.int) for sample in sample_y_train]
+        final = []
+        for sample in range(21):
+            final.append([data for freq_data in sample_y_train[sample] for data in freq_data])
+        sample_y_train = final
 
-        val_base_models = [model for freq_models in sample_models[-2:] for model in freq_models]
-        val_x_train = [data for freq_data in sample_x_train[-2:] for data in freq_data]
-        val_y_train = np.repeat(sample_y_train[-2:][:, np.newaxis], 15, axis=1)
-        val_y_train = [data for freq_data in val_y_train for data in freq_data]
-
-        base_models = [model for freq_models in sample_models[:-2] for model in freq_models]
-        x_train = [data for freq_data in sample_x_train[:-2] for data in freq_data]
-        y_train = np.repeat(sample_y_train[:-2][:, np.newaxis], 15, axis=1)
+        x_train = sample_predictions_proba[:-1]
+        x_train = [data for freq_data in x_train for data in freq_data]
+        print("length of x_train: {}".format(len(x_train)))
+        y_train = sample_y_train[:-1]
         y_train = [data for freq_data in y_train for data in freq_data]
+        # print("length of y_train: {}".format(len(y_train)))
+
+        x_val = sample_predictions_proba[-1:]
+        x_val = [data for freq_data in x_val for data in freq_data]
+        print("length of x_val: {}".format(len(x_val)))
+        y_val = sample_y_train[-1:]
+        y_val = [data for freq_data in y_val for data in freq_data]
+        # print("length of y_val: {}".format(len(y_val)))
 
         meta_model = LogisticRegression()
+        meta_model.fit(x_train, y_train)
 
-        sclf = StackingClassifier(classifiers=base_models, meta_classifier=meta_model, verbose=1,
-                                  fit_base_estimators=False)
-        sclf.fit(x_train, y_train)
+        print(meta_model.score(x_val, y_val))
 
-        print(sclf.score(val_x_train, val_y_train))
-
-        print("done")
 
 if __name__ == "__main__":
     main()
